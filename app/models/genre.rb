f2b1b3186755f876e09601314261e65731479969
tkyg <80114433+tkyg@users.noreply.github.com>
2022-11-10 14:53:11 -0500

    commit tree 84b886b16fa6a21a2d3b66b20bc168e4a3f1d3c1

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,20 +2,60 @@ class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
-  def song_count
-    # return the number of songs in a genre
-    self.songs.count
-  end
+  # def song_count
+  #   # return the number of songs in a genre
+  #   self.songs.count
+  # end
 
-  def artist_count
-    # return the number of artists associated with the genre
-    self.artists.count
-  end
+  # def artist_count
+  #   # return the number of artists associated with the genre
+  #   self.artists.count
+  # end
 
-  def all_artist_names
-    # return an array of strings containing every musician's name
-    self.artists.map do |artist|
-      artist.name
+  # def all_artist_names
+  #   # return an array of strings containing every musician's name
+  #   self.artists.map do |artist|
+  #     artist.name
+  #   end
+  # end
+
+    def song_count
+      self.songs.count
+    end
+
+    def artist_count
+      self.artists.count
+    end
+
+    def all_artist_names
+      self.artists.map do |artist|
+        artist.name
     end
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
